96c4979bdb0b44774dd9562c9b4b813c72b738c0
Fausto Ponce <104944931+Faustoronja@users.noreply.github.com>
2022-05-29 19:12:42 -0300

    Add files via upload

---
+++ Funcion dado.js
@@ -0,0 +1,18 @@
+export function rollthedice (max,min){
+    var numeroaleatorio = Math.round(Math.random()*(max-min))+min
+    return numeroaleatorio
+}
+
+export var juegooca = [
+"nada",
+"carbon",
+"carbon",
+"carbon",
+"La joya",
+"carbon",
+"carbon"
+]
+
+
+
+
